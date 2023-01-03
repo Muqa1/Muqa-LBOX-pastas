0ef6e669fdb3ff39e01b6a4c3393a76aa656f16f
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-01-03 19:32:07 +0200

    Update auto medic.lua

--- auto medic.lua
+++ auto medic.lua
@@ -1,9 +1,9 @@
 procent = 0.6 -- edit this value from 0.1 to 1 to change how low you must be to start the script (0.6 = 60% of your base health)
 
 -------------------------
-local LastExtenFreeze = 0 --pasted from someone, dont remember who tho
+local LastExtenFreeze = 0 
 local function MedicCall()
-    if gamecoordinator.IsConnectedToMatchServer() then -- just so it doesnt fill the console with "auto medic.lua:10: attempt to index a nil value (local 'LPlayer')" or sum like that
+    if gamecoordinator.IsConnectedToMatchServer() then 
 
     local LPlayer = entities.GetLocalPlayer()
 
