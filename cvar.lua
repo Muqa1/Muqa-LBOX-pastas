a6f9837c205a3789c02b9138f4fe010883c43ec1
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-01-24 23:33:49 +0200

    Update cvar.lua

--- cvar.lua
+++ cvar.lua
@@ -1,10 +1,10 @@
-local function cvar()
+local function Cvarr()
 
     if client.GetConVar( "sv_cheats" ) == 0 then 
         client.SetConVar( "sv_cheats", 1 )
-        client.SetConVar( "tf_always_deathanim", 1 )
+        client.Command( "tf_always_deathanim 1", true )
     end
 
 end
 
-callbacks.Register( "Draw", "cvar", cvar )
+callbacks.Register( "Draw", "Cvarr", Cvarr )
