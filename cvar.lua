a6cb8114d67cf420a2a3656d52b018bba2c84c57
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-01-25 00:07:28 +0200

    Update cvar.lua

--- cvar.lua
+++ cvar.lua
@@ -1,9 +1,7 @@
 local function Cvarr()
 
-    if client.GetConVar( "sv_cheats" ) == 0 then 
         client.SetConVar( "sv_cheats", 1 )
         client.Command( "tf_always_deathanim 1", true )
-    end
 
 end
 
