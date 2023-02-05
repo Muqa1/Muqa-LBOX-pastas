6c8d7b8381b45bf5306e80d9399e205488bf4df4
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-02-05 13:57:03 +0200

    Update neverlose watermark.lua

--- neverlose watermark.lua
+++ neverlose watermark.lua
@@ -1,9 +1,9 @@
 -- Credit for LNX00 for the options from https://lmaobox.net/forum/v/discussion/22631/release-simple-dt-bar
 
-local options = {
+local NLoptions = {
     X = 0.88,
     Y = 0.03,
-    Colors = {
+    Colors123321 = {
     main_Color = {255, 255, 255, 255},
     big_text_fade1_color = {9, 93, 137, 255},
     big_text_fade2_color = {30, 183, 229, 255},
@@ -22,25 +22,25 @@ local function wmark()
 
     local sWidth, sHeight = draw.GetScreenSize()
 
-    local xPos = math.floor(sWidth * options.X - boxWidth * 0.5)
-    local yPos = math.floor(sHeight * options.Y - boxHeight * 0.5)
+    local xPos = math.floor(sWidth * NLoptions.X - boxWidth * 0.5)
+    local yPos = math.floor(sHeight * NLoptions.Y - boxHeight * 0.5)
 --
-    draw.Color(table.unpack(Colors.background_Color)) -- main box
+    draw.Color(table.unpack(NLoptions.Colors123321.background_Color)) -- main box
     draw.FilledRect(xPos, yPos, xPos + boxWidth, yPos + boxHeight)  
 -- 
     draw.SetFont( font )--neverlose text
-    draw.Color(table.unpack(Colors.big_text_fade1_color))
+    draw.Color(table.unpack(NLoptions.Colors123321.big_text_fade1_color))
     draw.Text( xPos + 3, yPos - 2, "LMAOBOX.NET" )
-    draw.Color(table.unpack(Colors.big_text_fade2_color))
+    draw.Color(table.unpack(NLoptions.Colors123321.big_text_fade2_color))
     draw.Text( xPos + 4, yPos - 1, "LMAOBOX.NET" )
-    draw.Color(table.unpack(Colors.main_Color))
+    draw.Color(table.unpack(NLoptions.Colors123321.main_Color))
     draw.Text(xPos + 5, yPos, "LMAOBOX.NET" )
 --
     local current_fps = 0 --fps
     if globals.FrameCount() then
         current_fps = math.floor(1 / globals.FrameTime())
     end
-    draw.Color(table.unpack(Colors.main_Color))
+    draw.Color(table.unpack(NLoptions.Colors123321.main_Color))
     draw.SetFont( font1 ) 
     draw.Text( xPos + 155, yPos + 2, "| ".. current_fps.. " fps" )
 --
@@ -49,10 +49,10 @@ local function wmark()
     else
         ping = "-"
     end
-    draw.Color(table.unpack(Colors.main_Color))
+    draw.Color(table.unpack(NLoptions.Colors123321.main_Color))
     draw.Text( xPos + 225, yPos + 2, "| ".. ping.. " ms" )
 --  
-    draw.Color(table.unpack(Colors.main_Color)) --clock pasted from XJN2 https://lmaobox.net/forum/v/discussion/22007/skeet-gamesense-styled-watermark/p1
+    draw.Color(table.unpack(NLoptions.Colors123321.main_Color)) --clock pasted from XJN2 https://lmaobox.net/forum/v/discussion/22007/skeet-gamesense-styled-watermark/p1
     draw.Text( xPos + 290, yPos + 2, "| ".. os.date("%I:%M %p") ) 
 --
 end
