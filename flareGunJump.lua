7fc14f57ca272daaa6919529878720fe22c50261
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-03-12 03:08:10 +0200

    Create flareGunJump.lua

---
+++ flareGunJump.lua
@@ -0,0 +1,24 @@
+local function flareJump( cmd )
+    local player = entities.GetLocalPlayer( );
+
+    if (player ~= nil or not player:IsAlive()) then
+    end
+
+    if input.IsButtonDown( KEY_V ) then
+
+        local flags = player:GetPropInt( "m_fFlags" );
+
+        if (flags & FL_ONGROUND) and (flags & LOADOUT_POSITION_SECONDARY) then
+            cmd:SetButtons(cmd.buttons | IN_JUMP)
+            cmd:SetButtons(cmd.buttons | IN_DUCK)
+            cmd:SetButtons(cmd.buttons | IN_ATTACK)
+            cmd:SetViewAngles( 89, 0, 0 )
+        else 
+            cmd:SetButtons(cmd.buttons & ~IN_JUMP)
+            cmd:SetButtons(cmd.buttons & ~IN_DUCK)
+            cmd:SetButtons(cmd.buttons & ~IN_ATTACK)
+        end
+    end
+end
+
+callbacks.Register("CreateMove", "flareJump", flareJump)
