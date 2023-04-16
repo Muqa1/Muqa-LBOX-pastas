123622ab6626c161b541baf3e2df2da6d279623a
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-04-16 15:07:34 +0300

    Create trimping helper.lua

---
+++ trimping helper.lua
@@ -0,0 +1,25 @@
+local function TrimpHelper(cmd)
+if (entities.GetLocalPlayer():InCond(17)) then
+    gui.SetValue( "Auto Strafe", 0 )
+    pitch, yaw, roll = cmd:GetViewAngles()
+    if input.IsButtonDown( KEY_D ) then 
+        cmd:SetViewAngles( pitch, yaw - 55, roll )
+        cmd:SetButtons(cmd.buttons | IN_FORWARD)
+        client.Command( "-moveright", 1 )
+    else
+        cmd:SetButtons(cmd.buttons & (~IN_FORWARD))
+        client.Command( "-moveright", 0 )
+    end
+    if input.IsButtonDown( KEY_A ) then 
+        cmd:SetViewAngles( pitch, yaw + 55, roll )
+        cmd:SetButtons(cmd.buttons | IN_FORWARD)
+        client.Command( "-moveleft", 1 )
+    else
+        cmd:SetButtons(cmd.buttons & (~IN_FORWARD))
+        client.Command( "-moveleft", 0 )
+    end
+else 
+    gui.SetValue( "Auto Strafe", 2 )
+end
+end
+callbacks.Register( "CreateMove", "TrimpHelper", TrimpHelper )
