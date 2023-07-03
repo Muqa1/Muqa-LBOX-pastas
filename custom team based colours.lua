af106bc5bfef120bede75ac459ba788bd93a770a
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-07-04 02:48:05 +0300

    Update custom team based colours.lua
    made editing the colors hopefully easier for ppl who don know lua
    
    added antiaim indicator color customization

--- custom team based colours.lua
+++ custom team based colours.lua
@@ -1,3 +1,24 @@
+-- edit the numbers to change the colours, the format is rgba (RED, GREEN, BLUE, ALPHA)
+
+enemy = {82, 163, 255, 255}
+enemyInvisible = {51, 101, 158, 255}
+
+friendly = {255, 204, 110, 255}
+friendlyInvisible = {150, 120, 65, 255}
+
+menu = {82, 163, 255, 255}
+
+AimbotTarget = {201, 222, 255, 255}
+
+nightmode = {230, 201, 255, 255}
+
+backtrack = {82, 163, 255, 255}
+
+AntiAimIndicator = {255, 255, 255, 255}
+
+
+--====================================================================================--
+-- dont touch anything from down here unless you know what ur doing
 function rgbaToHex(r, g, b, a)
     r = math.floor(r)
     g = math.floor(g)
@@ -5,52 +26,35 @@ function rgbaToHex(r, g, b, a)
     a = math.floor(a)
     return tonumber("0x".. string.format("%02x%02x%02x%02x", r, g, b, a))
 end
-
--- edit the numbers to change the colours, the format is (RED, GREEN, BLUE, ALPHA)
-
-enemy = rgbaToHex(82, 163, 255, 255) 
-enemyInvisible = rgbaToHex(51, 101, 158, 255) 
-
-friendly = rgbaToHex(255, 204, 110, 255)
-friendlyInvisible = rgbaToHex(150, 120, 65, 255) 
-
-menu = rgbaToHex(82, 163, 255, 255)
-
-AimbotTarget = rgbaToHex(201, 222, 255, 255)
-
-nightmode = rgbaToHex(230, 201, 255, 255) 
-
-backtrack = rgbaToHex(82, 163, 255, 255) 
-
---------
-
+enemy_hex = rgbaToHex(table.unpack(enemy)) 
+enemyInvisible_hex = rgbaToHex(table.unpack(enemyInvisible)) 
+friendly_hex = rgbaToHex(table.unpack(friendly))
+friendlyInvisible_hex = rgbaToHex(table.unpack(friendlyInvisible)) 
+menu_hex = rgbaToHex(table.unpack(menu))
+AimbotTarget_hex = rgbaToHex(table.unpack(AimbotTarget))
+nightmode_hex = rgbaToHex(table.unpack(nightmode)) 
+backtrack_hex = rgbaToHex(table.unpack(backtrack)) 
+AntiAimIndicator_hex = rgbaToHex(table.unpack(AntiAimIndicator)) 
 local function colors()
 if engine.IsGameUIVisible() == false then
-    local player = entities.GetLocalPlayer()
-    
-    local myteam = player:GetTeamNumber()
+    if entities.GetLocalPlayer():GetTeamNumber() == 2 then 
+        gui.SetValue("blue team color", enemy_hex) -- enemy 
+        gui.SetValue("blue team (invisible)", enemyInvisible_hex)
 
-    if myteam == 2 then 
-        gui.SetValue("blue team color", enemy) -- enemy 
-        gui.SetValue("blue team (invisible)", enemyInvisible)
-
-        gui.SetValue("red team (invisible)", friendlyInvisible)
-        gui.SetValue( "red team color", friendly) --friendly 
+        gui.SetValue("red team (invisible)", friendlyInvisible_hex)
+        gui.SetValue( "red team color", friendly_hex) --friendly 
     else
-        gui.SetValue("blue team color", friendly) --friendly
-        gui.SetValue("blue team (invisible)", friendlyInvisible)
+        gui.SetValue("blue team color", friendly_hex) --friendly
+        gui.SetValue("blue team (invisible)", friendlyInvisible_hex)
 
-        gui.SetValue("red team (invisible)", enemyInvisible)
-        gui.SetValue( "red team color", enemy) -- enemy
+        gui.SetValue("red team (invisible)", enemyInvisible_hex)
+        gui.SetValue( "red team color", enemy_hex) -- enemy
     end
-    
-    gui.SetValue( "gui color", menu) 
-
-    gui.SetValue( "aimbot target color", AimbotTarget )
-    
-    gui.SetValue( "night mode color", nightmode )
-
-    gui.SetValue( "backtrack ticks color", backtrack )
+    gui.SetValue( "gui color", menu_hex) 
+    gui.SetValue( "aimbot target color", AimbotTarget_hex)
+    gui.SetValue( "night mode color", nightmode_hex)
+    gui.SetValue( "backtrack ticks color", backtrack_hex)
+    gui.SetValue( "Anti aim indicator color", AntiAimIndicator_hex)
     end
 end
-callbacks.Register( "Draw", colors)
+callbacks.Register( "Draw", "colors", colors)
