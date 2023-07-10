5f74bd308f38a78364d4b8976ade9ccbacb07230
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-07-11 02:48:04 +0300

    Update projectile esp.lua

--- projectile esp.lua
+++ projectile esp.lua
@@ -1,7 +1,12 @@
+local Enemy_only = false
+
+local Max_dist = 2800 -- hammer units
+
 local Proj_name = true
 local Proj_dist = true
 local Proj_box = true
-local Proj_color = { 255, 255, 255, 255 }
+local Proj_color_enemy = { 255, 255, 255, 255 }
+local Proj_color_friendly = { 0, 255, 0, 255 }
 
 --======================================================================================================================--
 local font = draw.CreateFont( "Tahoma", 12, 800, FONTFLAG_OUTLINE  )
@@ -30,19 +35,19 @@ local function proj_esp()
                 ["CTFProjectile_MechanicalArmOrb"] = "Orb",
                 ["CTFProjectile_HealingBolt"] = "Healing Arrow",
                 ["CTFProjectile_Jar"] = "Jarate",
-                ["CTFProjectile_JarGas"] = "Gas Passer"
+                ["CTFProjectile_JarGas"] = "Gas Passer",
+                ["CTFAmmoPack"] = "Ammo Pack",
+                ["CTFDroppedWeapon"] = "Dropped Weapon"
             }
 
             local entity = entities.FindByClass( entity_name )
             for i, projectile in pairs(entity) do 
                 local projectile_screen = client.WorldToScreen( projectile:GetAbsOrigin() )
-                if projectile_screen ~= nil and projectile:IsDormant() == false and projectile:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber() then  -- and projectile:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber()
-                    draw.Color(table.unpack(Proj_color))
-                    if Proj_box == true then -- pasted 3d box B)
+                if projectile_screen ~= nil and projectile:IsDormant() == false and (not Enemy_only or projectile:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber()) and vector.Distance( entities.GetLocalPlayer():GetAbsOrigin(), projectile:GetAbsOrigin() ) < Max_dist then -- and projectile:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber()
+                    if Proj_box == true then
                         local hitboxes = projectile:HitboxSurroundingBox()
                         local min = hitboxes[1]
                         local max = hitboxes[2]
-                        -- Define the eight vertices of the box
                         local vertices = {
                             Vector3(min.x, min.y, min.z),
                             Vector3(min.x, max.y, min.z),
@@ -53,7 +58,6 @@ local function proj_esp()
                             Vector3(max.x, max.y, max.z),
                             Vector3(max.x, min.y, max.z)
                         }
-                        -- Convert vertices to screen space
                         local screenVertices = {}
                         for j, vertex in ipairs(vertices) do
                             local screenPos = client.WorldToScreen(vertex)
@@ -61,8 +65,11 @@ local function proj_esp()
                                 screenVertices[j] = {x = screenPos[1], y = screenPos[2]}
                             end
                         end
-                        -- Draw the 3D box
-                        draw.Color(table.unpack(Proj_color))
+                        if Enemy_only == false and projectile:GetTeamNumber() == entities.GetLocalPlayer():GetTeamNumber() then 
+                            draw.Color(table.unpack(Proj_color_friendly))
+                        else 
+                            draw.Color(table.unpack(Proj_color_enemy))
+                        end
                         for j = 1, 4 do
                             local vertex1 = screenVertices[j]
                             local vertex2 = screenVertices[j % 4 + 1]
@@ -82,8 +89,14 @@ local function proj_esp()
                         end                 
                     end
                     if Proj_name == true then
-                        local projectile_length, projectile_height = draw.GetTextSize(esp_name[entity_name])
-                        draw.Text( projectile_screen[1] - math.floor(projectile_length / 2), projectile_screen[2] - 20, esp_name[entity_name])
+                        local draw_name
+                        if esp_name[entity_name] == nil then -- just so if u add any new entities u dont get any name errors
+                            draw_name = tostring(projectile:GetClass())
+                        else
+                            draw_name = esp_name[entity_name]
+                        end 
+                        local projectile_length, projectile_height = draw.GetTextSize(draw_name)
+                        draw.Text( projectile_screen[1] - math.floor(projectile_length / 2), projectile_screen[2] - 20, draw_name)
                     end
                     if Proj_dist == true then
                         local projectile_dist = vector.Distance( entities.GetLocalPlayer():GetAbsOrigin(), projectile:GetAbsOrigin() )
@@ -95,16 +108,26 @@ local function proj_esp()
             end
         end
 
-        --[[ script for getting all of the entities
+        --[[ script i use to get the entities
+
         for i = 1, 8192 do
             local entity = entities.GetByIndex( i )
             if entity then
                 print("Index: ".. i.. " | Entity name: ".. entity:GetClass() )
             end
         end
+        local me = entities.GetLocalPlayer()
+        local source = me:GetAbsOrigin() + me:GetPropVector( "localdata", "m_vecViewOffset[0]" )
+        local destination = source + engine.GetViewAngles():Forward() * 1000
+        local trace = engine.TraceLine( source, destination, MASK_SHOT_HULL )
+        if (trace.entity ~= nil) then
+            print( "Looking at: " .. trace.entity:GetClass() )
+        end
+        
         ]]
 
         -- all projectiles that i found
+        -- also some other cool stuff
 
         draw_esp("CTFStunBall") -- sandman
         draw_esp("CTFBall_Ornament") -- wrap assasin
@@ -123,6 +146,8 @@ local function proj_esp()
         draw_esp("CTFProjectile_Jar") -- jarate
         draw_esp("CTFProjectile_JarGas") -- gas passer
         draw_esp("CTFProjectile_SentryRocket") -- sentry rocket
+        -- draw_esp("CTFAmmoPack") -- dropped ammo packs
+        -- draw_esp("CTFDroppedWeapon") -- dropped weapon
     end
 end
 callbacks.Register( "Draw", "proj_esp", proj_esp )
