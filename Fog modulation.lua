a29a717c3ebccf7e92ca2d8db04bdb433d01e6fd
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2023-09-05 17:58:42 +0300

    Update Fog modulation.lua

--- Fog modulation.lua
+++ Fog modulation.lua
@@ -96,7 +96,7 @@ callbacks.Register( "Draw", function()
             f:SetPropInt(0, "m_fog.enable")
         end
         f:SetPropFloat(settings.density / 100, "m_fog.maxdensity")
-        local rgb = (settings.color_r) | (settings.color_g << 8) | settings.color_b << 16 -- idk why the rgb has to be backwards to work
+        local rgb = (settings.color_r) | (settings.color_g << 8) | settings.color_b << 16
         f:SetPropInt(rgb , "m_fog.colorPrimary")
         f:SetPropFloat(settings.fogStart, "m_fog.start")
         f:SetPropFloat(settings.fogEnd, "m_fog.end")
