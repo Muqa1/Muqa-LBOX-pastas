0d29d709ca686a3cb32019243972f4898c635633
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2023-09-20 20:31:16 +0300

    Update Misc Tools.lua

--- Misc Tools.lua
+++ Misc Tools.lua
@@ -15,6 +15,8 @@ local config = {
         custom_spin = false,
         custom_spin_invert = false,
         custom_spin_speed = 5,
+        custom_spin_rand_speed = true,
+        custom_spin_rand_invert = true,
 
         rotate_dynamic = false,
         rotate_dynamic_fake1 = 90,
@@ -60,6 +62,10 @@ local config = {
         },
 
         aa_lines = false,
+        aa_info = {
+            boring_lines = false,
+            size = 20
+        },
 
         damage_logger = false,
         damage_logger_info = {
@@ -199,6 +205,89 @@ local function LerpBetweenColors(color1, color2, frequency)
     return r, g, b
 end
 
+local function antiaimCross(localplayer_pos, aa_angle, size)
+    local vwA = engine.GetViewAngles()
+    if not aa_angle then return end
+    local dir = Vector3(math.cos(math.rad(vwA.y + aa_angle)), math.sin(math.rad(vwA.y + aa_angle)), 0)
+    local p = client.WorldToScreen(localplayer_pos)
+    if p ~= nil then
+        local a1 = localplayer_pos + dir * size
+        local a2 = localplayer_pos + dir
+        local a3 = localplayer_pos + dir * (size * 3)
+        local a4 = localplayer_pos + dir * (size * 3)
+        local a5 = localplayer_pos + dir * (size * 4.5)
+        local a6 = localplayer_pos + dir * (size * 5.5)
+        local b1 = client.WorldToScreen(a1)
+        if b1 ~= nil then
+            local pD = Vector3(-dir.y, dir.x, 0)
+            local r = size * 0.75
+            local c1 = a2 + pD * r
+            local c2 = a2 - pD * r 
+            local c3 = a3 + pD * r
+            local c4 = a3 - pD * r
+            local c5 = a4 + pD * (r * 2.5)
+            local c6 = a4 - pD * (r * 2.5)
+            local c7 = a5 + pD * (r * 2.5)
+            local c8 = a5 - pD * (r * 2.5)
+            local c9 = a6 + pD * r
+            local c10 = a6 - pD * r
+            local c11 = a5 + pD * r
+            local c12 = a5 - pD * r
+            local d1 = client.WorldToScreen(c1)
+            local d2 = client.WorldToScreen(c2)
+            local d3 = client.WorldToScreen(c3)
+            local d4 = client.WorldToScreen(c4)
+            local d5 = client.WorldToScreen(c5)
+            local d6 = client.WorldToScreen(c6)
+            local d7 = client.WorldToScreen(c7)
+            local d8 = client.WorldToScreen(c8)
+            local d9 = client.WorldToScreen(c9)
+            local d10 = client.WorldToScreen(c10)
+            local d11 = client.WorldToScreen(c11)
+            local d12 = client.WorldToScreen(c12)
+            if d1 and d2 and d3 and d4 and d5 and d6 and d7 and d8 and d9 and d10 and d11 and d12 then
+                draw.Line(d1[1], d1[2], d2[1], d2[2])
+                draw.Line(d1[1], d1[2], d3[1], d3[2])
+                draw.Line(d2[1], d2[2], d4[1], d4[2])
+                draw.Line(d4[1], d4[2], d6[1], d6[2])
+                draw.Line(d3[1], d3[2], d5[1], d5[2])
+                draw.Line(d5[1], d5[2], d7[1], d7[2])
+                draw.Line(d6[1], d6[2], d8[1], d8[2])
+                draw.Line(d8[1], d8[2],d12[1], d12[2])
+                draw.Line(d7[1], d7[2],d11[1], d11[2])
+                draw.Line(d9[1], d9[2],d11[1], d11[2])
+                draw.Line(d10[1], d10[2],d12[1], d12[2])
+                draw.Line(d9[1], d9[2], d10[1], d10[2])
+            end
+        end
+    end
+end
+
+local function antiaimArrow(localplayer_pos, aa_angle, range)
+    local vwA = engine.GetViewAngles()
+    if not aa_angle then return end
+    local direction = Vector3(math.cos(math.rad(vwA.y + aa_angle)), math.sin(math.rad(vwA.y + aa_angle)), 0)
+    local screenPos = client.WorldToScreen(localplayer_pos)
+    if screenPos ~= nil then
+        local endPoint = localplayer_pos + direction * range
+        local endPoint2 = localplayer_pos + direction * (range * 0.85)
+        local screenPos1 = client.WorldToScreen(endPoint)
+        if screenPos1 ~= nil then
+            draw.Line(screenPos[1], screenPos[2], screenPos1[1], screenPos1[2])
+            local perpendicularDirection = Vector3(-direction.y, direction.x, 0)
+            local perpendicularEndPoint1 = endPoint2 + perpendicularDirection * (range * 0.1) 
+            local perpendicularEndPoint2 = endPoint2 - perpendicularDirection * (range * 0.1) 
+            local screenPos2 = client.WorldToScreen(perpendicularEndPoint1)
+            local screenPos3 = client.WorldToScreen(perpendicularEndPoint2)
+            if screenPos2 ~= nil and screenPos3 ~= nil then
+                draw.Line(screenPos2[1], screenPos2[2], screenPos3[1], screenPos3[2])
+                draw.Line(screenPos1[1], screenPos1[2], screenPos3[1], screenPos3[2])
+                draw.Line(screenPos1[1], screenPos1[2], screenPos2[1], screenPos2[2])
+            end
+        end
+    end
+end
+
 local logs = {}
 
 local delays = {
@@ -209,6 +298,8 @@ local delays = {
     spy_warning = 0,
     rotate_dynamic_wait = 0,
     fine_shot_m8 = 0,
+    rand_invert = 0,
+    rand_spin = 0,
 }
 
 local function MiscDraw()
@@ -531,39 +622,21 @@ local function MiscDraw()
         }
 
         local center = localPlayer:GetAbsOrigin()
-        local range = 50
+        local range = 100
         local ang = engine.GetViewAngles()
 
 
-        draw.Color(0, 255, 0, 255) -- real
-        local yaw = yaws_real[gui.GetValue("anti aim - yaw (real)")]
-        if yaw then
-            local direction = Vector3(math.cos(math.rad(ang.y + yaw)), math.sin(math.rad(ang.y + yaw)), 0)
-            local direction = direction
-            local screenPos = client.WorldToScreen(center)
-            if screenPos ~= nil then
-                local endPoint = center + direction * range
-                local screenPos1 = client.WorldToScreen(endPoint)
-                if screenPos1 ~= nil then
-                    draw.Line(screenPos[1], screenPos[2], screenPos1[1], screenPos1[2])
-                end
-            end
-        end
-    
 
-        draw.Color(255, 0, 0, 255) -- real
-        local yaw = yaws_fake[gui.GetValue("anti aim - yaw (fake)")]
-        if yaw then
-            local direction = Vector3(math.cos(math.rad(ang.y + yaw)), math.sin(math.rad(ang.y + yaw)), 0)
-            local direction = direction
-            local screenPos = client.WorldToScreen(center)
-            if screenPos ~= nil then
-                local endPoint = center + direction * range
-                local screenPos1 = client.WorldToScreen(endPoint)
-                if screenPos1 ~= nil then
-                    draw.Line(screenPos[1], screenPos[2], screenPos1[1], screenPos1[2])
-                end
-            end
+        if config.visuals.aa_info.boring_lines then 
+            draw.Color(255, 0, 0, 255)
+            antiaimArrow(localPlayer:GetAbsOrigin(), yaws_fake[gui.GetValue("Anti aim - yaw (fake)")], config.visuals.aa_info.size)
+            draw.Color(0, 255, 0, 255)
+            antiaimArrow(localPlayer:GetAbsOrigin(), yaws_real[gui.GetValue("Anti aim - yaw (real)")], config.visuals.aa_info.size)
+        else
+            draw.Color(255, 0, 0, 255)
+            antiaimCross(localPlayer:GetAbsOrigin(), yaws_fake[gui.GetValue("Anti aim - yaw (fake)")], config.visuals.aa_info.size)
+            draw.Color(0, 255, 0, 255)
+            antiaimCross(localPlayer:GetAbsOrigin(), yaws_real[gui.GetValue("Anti aim - yaw (real)")], config.visuals.aa_info.size)
         end
 
     end
@@ -700,10 +773,13 @@ local function MiscDraw()
                 config.antiaim.rotate_dynamic_fake1 = ImMenu.Slider("Fake Angle #1", config.antiaim.rotate_dynamic_fake1 , -180, 180)
                 ImMenu.EndFrame()
                 ImMenu.BeginFrame(1)
-                config.antiaim.rotate_dynamic_fake2 = ImMenu.Slider("Fake Angle #2", config.antiaim.rotate_dynamic_fake2 , -180, 180)
+                config.antiaim.rotate_dynamic_real1 = ImMenu.Slider("Real Angle #1", config.antiaim.rotate_dynamic_real1 , -180, 180)
                 ImMenu.EndFrame()
                 ImMenu.BeginFrame(1)
-                config.antiaim.rotate_dynamic_real1 = ImMenu.Slider("Real Angle #1", config.antiaim.rotate_dynamic_real1 , -180, 180)
+                ImMenu.Text(" ")
+                ImMenu.EndFrame()
+                ImMenu.BeginFrame(1)
+                config.antiaim.rotate_dynamic_fake2 = ImMenu.Slider("Fake Angle #2", config.antiaim.rotate_dynamic_fake2 , -180, 180)
                 ImMenu.EndFrame()
                 ImMenu.BeginFrame(1)
                 config.antiaim.rotate_dynamic_real2 = ImMenu.Slider("Real Angle #2", config.antiaim.rotate_dynamic_real2 , -180, 180)
@@ -717,6 +793,10 @@ local function MiscDraw()
                 ImMenu.BeginFrame(1)
                 config.antiaim.custom_spin_invert = ImMenu.Checkbox("Invert", config.antiaim.custom_spin_invert)
                 config.antiaim.custom_spin_speed = ImMenu.Slider("Spin Speed", config.antiaim.custom_spin_speed , 1, 10)
+                ImMenu.EndFrame()
+                ImMenu.BeginFrame(1)
+                config.antiaim.custom_spin_rand_speed = ImMenu.Checkbox("Random Speed", config.antiaim.custom_spin_rand_speed)
+                config.antiaim.custom_spin_rand_invert = ImMenu.Checkbox("Random Invert", config.antiaim.custom_spin_rand_invert)
                 ImMenu.EndFrame()
             end
 
@@ -759,7 +839,22 @@ local function MiscDraw()
 
             if config.visuals.damage_logger then 
                 ImMenu.BeginFrame(1)
-                config.visuals.damage_logger_info.damage_logger_movable = ImMenu.Checkbox("Damage Logger Movable", config.visuals.damage_logger_info.damage_logger_movable)
+                ImMenu.Text("DMG Logger Settings")
+                ImMenu.EndFrame()
+                ImMenu.BeginFrame(1)
+                config.visuals.damage_logger_info.damage_logger_movable = ImMenu.Checkbox("Custom DMG Logger Pos", config.visuals.damage_logger_info.damage_logger_movable)
+                ImMenu.EndFrame()
+            end
+
+            if config.visuals.aa_lines then  
+                ImMenu.BeginFrame(1)
+                ImMenu.Text("AA Lines Settings")
+                ImMenu.EndFrame()
+                ImMenu.BeginFrame(1)
+                config.visuals.aa_info.boring_lines = ImMenu.Checkbox("Boring AA Lines", config.visuals.aa_info.boring_lines)
+                ImMenu.EndFrame()
+                ImMenu.BeginFrame(1)
+                config.visuals.aa_info.size = ImMenu.Slider("AA Line Size", config.visuals.aa_info.size , 1, 100)
                 ImMenu.EndFrame()
             end
 
@@ -800,10 +895,12 @@ local function MiscDraw()
             ImMenu.BeginFrame(1)
             if ImMenu.Button("Create/Save CFG") then
                 CreateCFG( [[Misc Tools Lua]] , config )
+                printc(255, 174, 0, 255, "Saved Lua Config")
             end
 
             if ImMenu.Button("Load CFG") then
                 config = LoadCFG( [[Misc Tools Lua]] )
+                printc(0, 255, 136, 255, "Loaded Lua Config")
             end
 
             ImMenu.EndFrame()
@@ -823,20 +920,27 @@ local function MiscCreateMove(cmd)
 
 
     if config.antiaim.custom_spin then 
-        if not config.antiaim.custom_spin_invert then
-        
-            gui.SetValue( "Anti Aim - Custom Yaw (real)", gui.GetValue( "Anti Aim - Custom Yaw (real)" ) + config.antiaim.custom_spin_speed)
-
-            if (gui.GetValue( "Anti Aim - Custom Yaw (real)") >= 170) then 
-                gui.SetValue( "Anti Aim - Custom Yaw (real)", -180)
+        local add = config.antiaim.custom_spin_speed
+        local function a(x)
+            if x >= 180 then
+                x = -180
+            elseif x <= -180 then
+                x = 180
             end
-        else
-            gui.SetValue( "Anti Aim - Custom Yaw (real)", gui.GetValue( "Anti Aim - Custom Yaw (real)" ) - config.antiaim.custom_spin_speed)
-  
-            if (gui.GetValue( "Anti Aim - Custom Yaw (real)") <= -170) then 
-                gui.SetValue( "Anti Aim - Custom Yaw (real)", 180)
-            end
+            return x
         end
+        if config.antiaim.custom_spin_rand_speed and globals.RealTime() > delays.rand_spin + 0.2 then 
+            config.antiaim.custom_spin_speed = math.random(1, 10)
+            delays.rand_spin = globals.RealTime()
+        end
+        if config.antiaim.custom_spin_rand_invert and globals.RealTime() > delays.rand_invert + 0.2 then 
+            config.antiaim.custom_spin_invert = (math.random(0,1)==1)
+            delays.rand_invert = globals.RealTime()
+        end
+        if config.antiaim.custom_spin_invert then
+            add = -add
+        end
+        gui.SetValue( "Anti Aim - Custom Yaw (real)", a(gui.GetValue( "Anti Aim - Custom Yaw (real)" ) + add))
     end
 
 
