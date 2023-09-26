92b3f7f48389239bd801ae084ca56133cbc0b436
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2023-09-26 22:43:31 +0300

    Create priority hitsounds.lua

---
+++ priority hitsounds.lua
@@ -0,0 +1,20 @@
+local function f(event)
+    local lPlr = entities.GetLocalPlayer()
+    if (event:GetName()=="player_hurt") then
+        local v=entities.GetByUserID(event:GetInt("userid"))
+        local h=event:GetInt("health")
+        local a=entities.GetByUserID(event:GetInt("attacker"))
+        if (a==nil or lPlr:GetIndex()~=a:GetIndex()) then return end
+        if playerlist.GetPriority(v)>0 then 
+            if h == 0 then 
+                engine.PlaySound("replay/saved.wav") -- killsound
+            else
+                engine.PlaySound("replay/saved_take.wav") -- hitsound
+            end
+            client.Command( "tf_dingalingaling 0", true ) -- turning the hitsounds and killsounds off
+        else
+            client.Command( "tf_dingalingaling 1", true )
+        end
+    end
+end
+callbacks.Register("FireGameEvent", "exampledamageLogger", f)
